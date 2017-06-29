297aa9837291b34bc4dd9b830c3c484d7739656e
RaptorboxN <vanderburgnick@gmail.com>
2017-06-29 22:25:44 +0200

    Revert "fixed show"
    This reverts commit cb3ac4c55709da7c008c5249773b3888ea6e6404.

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -11,7 +11,7 @@
                         @foreach($threads as $thread)
                             <article>
                                 <h4>
-                                    <a href="{{ $thread->path() }}">
+                                    <a href="/threads/{{ $thread->path() }}">
                                         {{ $thread->title }}
                                     </a>
                                 </h4>
